818340283f749b1ed747f75067851c58e65ab66f
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2016-12-16 20:32:28 +1300

    Add initial files of redux tutorial

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const webpack = require( 'webpack' );
+const path = require( 'path' );
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    'babel-polyfill',
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './app.js',
+  ],
+  output: {
+    path: path.join( __dirname, 'dist' ),
+    filename: 'bundle.js',
+    publicPath: '/dist',
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loaders: ['babel'],
+      include: path.join( __dirname ),
+    }],
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
+};
