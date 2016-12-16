5dbfcfe8bd1a576e6939e95634ba104d194c9ebd
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2016-12-16 22:00:43 +1300

    Create minimal createStore from scratch

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
 import expect from 'expect';
 
+// actions that determine the store value
 const counter = ( state = 0, action ) => {
   switch ( action.type ) {
     case 'INCREMENT':
@@ -12,19 +13,54 @@ const counter = ( state = 0, action ) => {
   }
 };
 
+// createStore from scratch
+const createStore = ( reducer ) => {
+  let state;
+  let listeners = [];
+
+  // just return the state
+  const getState = () => { return state; };
+
+  const dispatch = ( action ) => {
+    // this changes the state value
+    state = reducer( state, action );
+    // after the state change, update all the listeners that were subscribed on the state
+    listeners.forEach( ( listener ) => { return listener(); } );
+  };
+
+  const subscribe = ( listener ) => {
+    // put new listeners in the array
+    listeners.push( listener );
+    // this unsubscribes the listener
+    return () => {
+      listeners = listeners.filter( ( l ) => { return l !== listener; } );
+    };
+  };
+
+  // this returns the initial value of the state
+  dispatch( {} );
+
+  return { getState, dispatch, subscribe };
+};
+
 const store = createStore( counter );
 
+// method called to change the value displayed on the body
 const render = () => {
   document.getElementById( 'root' ).innerText = store.getState();
 };
 
+// specify the change listener called every time an action is dispatched
 store.subscribe( render );
+// call on load to show value 0
 render();
 
+// add event handling on the document and use redux dispatch to update value
 document.addEventListener( 'click', () => {
   store.dispatch( { type: 'INCREMENT' } );
 } );
 
+// pass tests for the state actions
 expect(
   counter( 0, { type: 'INCREMENT' } ),
 ).toEqual( 1 );
