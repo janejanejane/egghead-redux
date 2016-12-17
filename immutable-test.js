a6d4993c842e4b0ee4ced455c5ea2bf97cf9b5f2
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2016-12-17 21:25:45 +1300

    Add immutable testing: concat, slice, spread operators

---
+++ immutable-test.js
@@ -0,0 +1,81 @@
+import deepFreeze from 'deep-freeze';
+import expect from 'expect';
+
+const addCounter = ( list ) => {
+  // ensure that the object is not mutated
+
+  // ES5 syntax
+  // return list.concat( 0 );
+
+  // ES6 syntax
+  return [...list, 0];
+};
+
+const removeCounter = ( list, index ) => {
+  // ES5 syntax
+  // return list
+  //   .slice( 0, index )
+  //   .concat( list.slice( index + 1 ) );
+
+  // ES6 syntax
+  return [
+    ...list.slice( 0, index ),
+    ...list.slice( index + 1 ),
+  ];
+};
+
+const incrementCounter = ( list, index ) => {
+  // ES5 syntax
+  // return list
+  //   .slice( 0, index )
+  //   .concat( [list[index] + 1] )
+  //   .concat( list.slice( index + 1 ) );
+
+  // ES6 syntax
+  return [
+    ...list.slice( 0, index ),
+    list[index] + 1,
+    ...list.slice( index + 1 ),
+  ];
+};
+
+const testAddCounter = () => {
+  const listBefore = [];
+  const listAfter = [0];
+
+  // freeze object so mutation is not allowed
+  deepFreeze( listBefore );
+
+  expect(
+    addCounter( listBefore ),
+  ).toEqual( listAfter );
+};
+
+const testRemoveCounter = () => {
+  const listBefore = [0, 10, 20];
+  const listAfter = [0, 20];
+
+  // freeze object so mutation is not allowed
+  deepFreeze( listBefore );
+
+  expect(
+    removeCounter( listBefore, 1 ),
+  ).toEqual( listAfter );
+};
+
+const testIncrementCounter = () => {
+  const listBefore = [0, 10, 20];
+  const listAfter = [0, 11, 20];
+
+  // freeze object so mutation is not allowed
+  deepFreeze( listBefore );
+
+  expect(
+    incrementCounter( listBefore, 1 ),
+  ).toEqual( listAfter );
+};
+
+testAddCounter();
+testRemoveCounter();
+testIncrementCounter();
+console.log( 'All tests passed.' );
