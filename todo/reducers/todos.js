e125b4d2ea175fb175e76a3f8ed8765b75eb819b
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2016-12-21 14:06:48 +1300

    Update store details based on route, not just filter in UI

--- todo/reducers/todos.js
+++ todo/reducers/todos.js
@@ -1,15 +1,14 @@
 import { combineReducers } from 'redux';
-import todo from './todo';
 
 // specify a database-like syntax of state
 const byId = ( state = {}, action ) => {
   switch ( action.type ) {
-    case 'ADD_TODO':
-    case 'TOGGLE_TODO':
-      return {
-        ...state,
-        [action.id]: todo( state[action.id], action ),
-      };
+    case 'RECEIVE_TODOS':
+      const nexState = { ...state };
+      action.response.forEach( ( todo ) => {
+        nexState[todo.id] = todo;
+      } );
+      return nexState;
     default:
       return state;
   }
@@ -17,47 +16,61 @@ const byId = ( state = {}, action ) => {
 
 // manages all the ids
 const allIds = ( state = [], action ) => {
+  if ( action.filter !== 'all' ) {
+    return state;
+  }
+  // return data from server
   switch ( action.type ) {
-    case 'ADD_TODO':
-      return [...state, action.id];
+    case 'RECEIVE_TODOS':
+      return action.response.map( ( todo ) => { return todo.id; } );
     default:
       return state;
   }
 };
 
+const activeIds = ( state = [], action ) => {
+  if ( action.filter !== 'active' ) {
+    return state;
+  }
+  // return data from server
+  switch ( action.type ) {
+    case 'RECEIVE_TODOS':
+      return action.response.map( ( todo ) => { return todo.id; } );
+    default:
+      return state;
+  }
+};
+
+const completedIds = ( state = [], action ) => {
+  if ( action.filter !== 'completed' ) {
+    return state;
+  }
+  // return data from server
+  switch ( action.type ) {
+    case 'RECEIVE_TODOS':
+      return action.response.map( ( todo ) => { return todo.id; } );
+    default:
+      return state;
+  }
+};
+
+const idsByFilter = combineReducers( {
+  all: allIds,
+  active: activeIds,
+  completed: completedIds,
+} );
+
 const todos = combineReducers( {
   byId,
-  allIds,
+  idsByFilter,
 } );
 
 export default todos;
 
-// selector to get all the ids
-const getAllTodos = ( state ) => {
-  return state.allIds.map( ( id ) => {
-    return state.byId[id];
-  } );
-};
-
 // helps in filtering the todos based on the option clicked
 // filter value are based on url string
-export const getVisibleTodos = (
-  state,
-  filter,
-) => {
-  const allTodos = getAllTodos( state );
-  switch ( filter ) {
-    case 'all':
-      return allTodos;
-    case 'completed':
-      return allTodos.filter(
-        ( t ) => { return t.completed; },
-      );
-    case 'active':
-      return allTodos.filter(
-        ( t ) => { return !t.completed; },
-      );
-    default:
-      throw new Error( `Unknown filter: ${filter}.` );
-  }
+// avoid storing in-memory at once
+export const getVisibleTodos = ( state, filter ) => {
+  const ids = state.idsByFilter[filter];
+  return ids.map( ( id ) => { return state.byId[id]; } );
 };
